72eb750f42ada8258ea238ab4e310fda566e2b9e
fukumoto94 <31295382+fukumoto94@users.noreply.github.com>
2024-04-10 15:03:43 -0300

    Create actions.js

---
+++ js/actions.js
@@ -0,0 +1,25 @@
+document.getElementById('izaw1').addEventListener('click', function () {
+  // Obtenha todas as imagens dentro da div com id 'i78jh'
+  var imagens = document.querySelectorAll('#i78jh img');
+  // Encontre a imagem atualmente visível
+  var visivelAtual = Array.from(imagens).findIndex(function (imagem) {
+    return imagem.style.display !== 'none';
+  });
+  // Torne a imagem atual invisível
+  imagens[visivelAtual].style.display = 'none';
+  // Calcule o índice da próxima imagem
+  var proximoIndice = (visivelAtual + 1) % imagens.length;
+  // Torne a próxima imagem visível
+  imagens[proximoIndice].style.display = 'block';
+});
+
+document.getElementById('iyq4t').addEventListener('click', function () {
+  var imagens = document.querySelectorAll('#i78jh img');
+  var visivelAtual = Array.from(imagens).findIndex(function (imagem) {
+    return imagem.style.display !== 'none';
+  });
+  imagens[visivelAtual].style.display = 'none';
+  // Calcule o índice da imagem anterior
+  var anteriorIndice = (visivelAtual - 1 + imagens.length) % imagens.length;
+  imagens[anteriorIndice].style.display = 'block';
+});
